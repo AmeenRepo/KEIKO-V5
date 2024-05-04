a60cfcc3ca806bb8c618c011144e266ef842159e
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-05-04 23:10:02 +0530

    Add files via upload

---
+++ plugins/P-INSTALL.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
+
+let handler = async (m, { args, usedPrefix, command }) => {
+  if (!args) throw `*Give Me Plugin URL*`
+
+  // Extract the Gist ID from the URL
+  //const gistId = args.match(/(?:\/|gist\.github\.com\/)([AmeenRepo]+)/)
+
+//  if (!gistId) throw `Invalid plugin URL`
+
+ // const gistName = gistId[1]
+  const gistURL = args
+           if (!gistURL) throw `*Keiko V5 ERROR*`
+ 
+  try {
+    const response = await axios.get(gistURL)
+    const gistData = response.data
+
+    if (!gistData || !gistData.files) {
+      throw `No valid files found in the Gist`
+    }
+
+    for (const file of Object.values(gistData.files)) {
+      // Use the Gist file name as the plugin name
+      const pluginName = file.filename
+
+      // Construct the path to save the plugin
+      const pluginPath = path.join('plugins', `${pluginName}`)
+
+      // Write the Gist file content to the plugin file
+      await fs.promises.writeFile(pluginPath, file.content)
+      m.reply(`*PLUGIN INSTALLED✅*\n*Command:* _${pluginName}_\n> ©AmeenXnT`)
+    }
+  } catch (error) {
+    throw `Error fetching or saving the plugin: ${error.message}`
+  }
+}
+
+handler.help = ['$'].map(v => v + ' <Gist URL>')
+handler.tags = ['plugin']
+handler.command = /^plug$/i
+handler.prefix = false
+handler.owner = true
+
+export default handler
