11dbe8b275de2075edc6ca32a5d9d4862fbe8569
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-09-21 14:21:02 +0530

    Love 💕

--- plugins/vv.js
+++ plugins/vv.js
@@ -11,6 +11,6 @@ var handler = async (m, { conn }) => {
 
 handler.help = ['readvo']
 handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
+handler.command = ['readviewonce', 'read', 'vv', 'readvo'] 
 
 export default handler
