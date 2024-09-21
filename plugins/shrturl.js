65682f185350a75db7360dce161349a0161c32a5
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-09-21 13:43:37 +0530

    Update shrturl.js

--- plugins/shrturl.js
+++ plugins/shrturl.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, args, text }) => {
 
 handler.help = ['tinyurl', 'shorten'].map(v => v + ' <link>');
 handler.tags = ['tools'];
-handler.command = /^(tinyurl|short|acortar|corto)$/i;
+handler.command = /^(surl|turl)$/i;
 handler.fail = null;
 
 export default handler;
